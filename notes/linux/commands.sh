8f0a4025a28e800af50978a93ab5b1a940a9a225
s_sasaki <tack.main@gmail.com>
2014-09-18 00:14:23 +0900

    SSHメモ

---
+++ notes/linux/commands.sh
@@ -0,0 +1,10 @@
+
+# file mode for SSH
+chmod 755 /home/NAME/
+chmod 700 /home/NAME/.ssh/
+chmod 600 /home/NAME/.ssh/authorized_keys
+chmod 600 /home/NAME/.ssh/authorized_keys2
+chmod 600 /home/NAME/.ssh/id_rsa
+chmod 644 /home/NAME/.ssh/id_rsa.pub
+chmod 644 /home/NAME/.ssh/known_hosts
+
