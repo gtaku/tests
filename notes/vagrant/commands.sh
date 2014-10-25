7647c02afa0c0f473886fbf43f5194d791346b4c
s_sasaki <tack.main@gmail.com>
2014-10-25 23:03:06 +0900

    vagrantメモ

---
+++ notes/vagrant/commands.sh
@@ -0,0 +1,43 @@
+# バージョン確認
+vagrant -v
+
+# ヘルプ
+vagrant -h
+
+# Boxを追加
+vagrant box add NAME URL
+
+# Boxを確認
+vagrant box list
+
+# 仮想マシンの作成
+vagrant init BOX_NAME
+
+# 起動
+vagrant up
+
+# SSH
+vagrant ssh
+
+# スリープ
+vagrant suspend
+
+# スリープ復帰
+vagrant resume
+
+# 終了
+vagrant halt
+
+# 再起動
+vagrant reload
+
+# 削除
+vagrant destroy
+
+# 状態を確認
+vagrant status
+
+# Box作成
+vagrant package --output NAME
+
+
